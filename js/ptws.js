ee8480d6d950fd3f4ec0ff817338d77f3b0a5995
Garrett Birkel <gbirkel@me.com>
2018-08-20 01:42:19 -0700

    Override the "global caption module" extension to RoyalSlider with our own version, which uses "insertAfter" rather than "appendTo", effectively moving the global caption div outside the relatively-positioned slideshow, so our captions can have a height independent of the slides.

--- js/ptws.js
+++ js/ptws.js
@@ -60,6 +60,68 @@ function showGear(e) {
 
 
 jQuery(document).ready(function($) {
+
+	/**
+	 *
+	 * Override the "global caption module" extension to RoyalSlider
+	 * with our own version, which uses "insertAfter" rather than "appendTo",
+	 * effectively moving the global caption div outside the relatively-positioned slideshow,
+	 * so our captions can have a height independent of the slides.
+	 * Risky but effective.
+	 */
+	$.extend($.rsProto, {
+		_initGlobalCaption: function () {
+			var self = this;
+			if (self.st.globalCaption) {
+				var setCurrCaptionHTML = function () {
+
+					var gcc = self.ptwsGlobalCaptionContainer.get(0);
+					// Destroy any lingering/unfinished transitions
+					gcc.removeAttribute("style");
+					// Destroy any lingering/unfinished event listeners
+					self.ptwsGlobalCaptionContainer.unbind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd");
+					// Get the height of the caption box as-is
+					var start_h = gcc.offsetHeight;
+					// Fill the caption box with the new content
+					self.globalCaption.html(self.currSlide.caption || '');
+					// Force a calculation of the new height based on the new content
+					var end_h = gcc.offsetHeight;
+					// Set the caption box to the _old_ height explicitly 
+					gcc.style.height = start_h + 'px';
+					// Make sure the styling is recalculated as a static height (which we just set), rather than auto (which it was)
+					gcc.offsetHeight;
+					// Start a transition to the _new_ height explicitly
+					gcc.style.transition = "all 0.3s ease-out";
+					gcc.style.height = end_h + 'px';
+					// Set up a post-transition event to remove the explicit height
+					self.ptwsGlobalCaptionContainer.on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function () {
+						self.ptwsGlobalCaptionContainer.unbind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd");
+						var gcc = self.ptwsGlobalCaptionContainer.get(0);
+						// Blowing away the entire inline style ensures we return to CSS defaults,
+						// which effectively apply 'auto' for width and height.
+						// This is so the container can properly respond to changes in window/content size.
+						gcc.removeAttribute("style");
+						//  Trigger a fake "scroll" event so our lazy image loader can act on newly-visible images
+						// (This is a bit of a hack, placing it here in the caption module.)
+						ptws.fakeScroll();
+					});
+				};
+				self.ev.on('rsAfterInit', function () {
+					// Create our own custom container, outside the slideshow container (just after it in the DOM),
+					// and create the regular global caption container inside it.  The custom container is what we will use
+					// for dynamic resizing in setCurrCaptionHTML .
+					self.ptwsGlobalCaptionContainer = $('<div class="ptwsGCaptionContainer"></div>').insertAfter(!self.st.globalCaptionInside ? self.slider : self._sliderOverflow);
+					self.globalCaption = $('<div class="ptwsGCaption"></div>').appendTo(self.ptwsGlobalCaptionContainer);
+					setCurrCaptionHTML();
+				});
+				self.ev.on('rsBeforeAnimStart', function () {
+					setCurrCaptionHTML();
+				});
+			}
+		}
+	});
+	$.rsModules.globalCaption = $.rsProto._initGlobalCaption;
+
   $('div.royalSlider').royalSlider({
   	addActiveClass: true,
     arrowsNav: true,
