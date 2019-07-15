b45641a6641421124071c1de10825a080481abfd
Garrett Birkel <gbirkel@me.com>
2019-07-15 18:51:52 +0000

    Fixing global reference bug

--- ptws-libs.php
+++ ptws-libs.php
@@ -16,7 +16,7 @@ function ptws_create_afgFlickr_obj() {
 
 
 function ptws_epoch_to_str($e) {
-    $dt = new DateTime("@$e");  // convert UNIX timestamp to PHP DateTime
+    $dt = new \DateTime("@$e");  // convert UNIX timestamp to PHP DateTime
     return $dt->format('Y-m-d H:i:s'); // output = 2017-01-01 00:00:00
 }
 
