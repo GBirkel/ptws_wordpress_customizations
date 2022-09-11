b1877d890c45b17fffc971f515af4ad13304e6d9
Garrett Birkel <gbirkel@me.com>
2022-09-11 12:18:18 -0700

    Different webpack setup, updating all packages, removing many useless ones.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
 var path = require('path');
 var dir = 'js/dist';
 
 module.exports = {
 	entry: {
-    	'ptws': './js/src/ptws.ts'
+    	'ptws': [
+			path.resolve(__dirname, './js/src/block-itinerary.ts'),
+			path.resolve(__dirname, './js/src/ptws.ts')
+		]
    	},
 	output: {
    		path: path.join(__dirname, dir),
@@ -14,68 +15,55 @@ module.exports = {
       	library: 'ptws',
       	libraryTarget: 'window'
    	},
+	resolve: {
+        modules: [
+            path.resolve("node_modules"),
+            path.resolve(__dirname, './js/src/'),
+        ],
+        extensions: [ ".ts", ".js"]
+    },
 	devtool: "source-map",
 	module: {
 		rules: [ 
             {
                 test: /\.ts(x?)$/,
                 exclude: /node_modules/,
-                use: [
-                    {
-                        loader: "ts-loader"
-                    }
-                ]
+				use: ['ts-loader']
             },
-			{ 
-				test: /.jsx?$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/
-			},
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
-				loader: ['babel-loader', 'eslint-loader']
+				use:{ 
+				 loader:'babel-loader', 
+				 options:{
+				  presets:['@babel/preset-env'],
+				  plugins:['@babel/plugin-proposal-class-properties']
+				 }
+				}
 			},
 			{
 				test: /\.(jpe?g|gif|png|svg)$/,
-				loader: "file-loader",
-				options: {
-					name: 'img/[name].[ext]',
-					publicPath: '../'
-				}
+				use: {
+					loader:'file-loader', 
+					options: {
+						name: 'img/[name].[ext]',
+						publicPath: '../'
+					}
+				}   
 			},
 			{
 				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-				loader: 'file-loader',
-				options: {
-					name: 'fonts/[name].[ext]',
-					publicPath: '../'
-				}
+				use: {
+					loader:'file-loader', 
+					options: {
+						name: 'fonts/[name].[ext]',
+						publicPath: '../'
+					}
+				}   
 			},
 			{
-				test: /\.scss$/,
-				use: ExtractTextPlugin.extract({
-					fallback: "style-loader",
-					use: [
-						{ loader: 'css-loader',
-							options: {
-								sourceMap: true
-							}
-						},
-						{ loader: 'postcss-loader',
-							options: {
-								sourceMap: true
-							}
-						},
-						{ loader: 'sass-loader',
-							options: {
-								sourceMap: true,
-								outputStyle: 'expanded'
-							},
-						}
-					]
-				}),
-				exclude: /node_modules/,
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
 			},
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             {
@@ -86,19 +74,20 @@ module.exports = {
 		]
 	},
     externals: {
-		"react": "React",
+//		"react": "React",
         "jquery": "jQuery",
-		"@wordpress/components": { this: [ 'wp', 'components' ]},
-//		"@wordpress/element": { this: [ 'wp', 'element' ]},
-		"@wordpress/blocks": { this: [ 'wp', 'blocks' ]},
-		"@wordpress/editor": { this: [ 'wp', 'editor' ]},
-//		"@wordpress/utils": { this: [ 'wp', 'utils' ]},
-//		"@wordpress/date": { this: [ 'wp', 'date' ]},
-//		"@wordpress/data": { this: [ 'wp', 'data' ]},
-//		"@wordpress/compose": { this: [ 'wp', 'compose' ]}
+		"@wordpress/block-editor": 'wp.blockEditor',
+		"@wordpress/components": 'wp.components',
+		"@wordpress/element": 'wp.element',
+		"@wordpress/blocks": 'wp.blocks',
+		"@wordpress/editor": 'wp.editor',
+		"@wordpress/utils": 'wp.utils',
+		"@wordpress/date": 'wp.date',
+		"@wordpress/data": 'wp.data',
+		"@wordpress/compose": 'wp.compose',
+		"@wordpress/i18n": 'wp.i18n'
     },
 	plugins: [
-		new ExtractTextPlugin({ filename: 'css/[name].css' }),
 		new webpack.ProvidePlugin({
 			'ptws': 'ptws'
 		})
