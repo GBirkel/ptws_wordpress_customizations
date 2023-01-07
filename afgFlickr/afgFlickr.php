daf13b9d7b8e33ab35b72b8e48bc10ef2ca7f0da
Garrett Birkel <gbirkel@me.com>
2023-01-07 14:10:43 -0800

    Removing flickr cache code. Not using it.

--- afgFlickr/afgFlickr.php
+++ afgFlickr/afgFlickr.php
@@ -29,31 +29,13 @@ if ( !class_exists('afgFlickr') ) {
         var $req;
         var $response;
         var $parsed_response;
-        var $cache = false;
-        var $cache_db = null;
-        var $cache_table = null;
-        var $cache_dir = null;
-        var $cache_expire = null;
-        var $cache_key = null;
         var $last_request = null;
         var $die_on_error;
         var $error_code;
         Var $error_msg;
         var $token;
         var $php_version;
-        var $custom_post = null, $custom_cache_get = null, $custom_cache_set = null;
-
-        /*
-         * When your database cache table hits this many rows, a cleanup
-         * will occur to get rid of all of the old rows and cleanup the
-         * garbage in the table.  For most personal apps, 1000 rows should
-         * be more than enough.  If your site gets hit by a lot of traffic
-         * or you have a lot of disk space to spare, bump this number up.
-         * You should try to set it high enough that the cleanup only
-         * happens every once in a while, so this will depend on the growth
-         * of your table.
-         */
-        var $max_cache_rows = 1000;
+        var $custom_post = null;
 
 
         public function __construct($api_key, $secret = NULL, $die_on_error = false) {
@@ -79,133 +61,10 @@ if ( !class_exists('afgFlickr') ) {
         }
 
 
-        function enableCache ($type, $connection, $cache_expire = 600, $table = 'flickr_cache') {
-            // Turns on caching.  $type must be either "db" (for database caching) or "fs" (for filesystem).
-            // When using db, $connection must be a PEAR::DB connection string. Example:
-            //	  "mysql://user:password@server/database"
-            // If the $table, doesn't exist, it will attempt to create it.
-            // When using file system, caching, the $connection is the folder that the web server has write
-            // access to. Use absolute paths for best results.  Relative paths may have unexpected behavior
-            // when you include this.  They'll usually work, you'll just want to test them.
-            if ($type == 'db') {
-                if ( preg_match('|mysql://([^:]*):([^@]*)@([^/]*)/(.*)|', $connection, $matches) ) {
-                    //Array ( [0] => mysql://user:password@server/database [1] => user [2] => password [3] => server [4] => database ) 
-                    $db = mysql_connect($matches[3], $matches[1], $matches[2]);
-                    mysql_select_db($matches[4], $db);
-
-                    /*
-                     * If high performance is crucial, you can easily comment
-                     * out this query once you've created your database table.
-                     */
-                    mysql_query("
-                        CREATE TABLE IF NOT EXISTS `$table` (
-                            `request` CHAR( 35 ) NOT NULL ,
-                            `response` MEDIUMTEXT NOT NULL ,
-                            `expiration` DATETIME NOT NULL ,
-                            INDEX ( `request` )
-                        ) TYPE = MYISAM
-                        ", $db);
-
-                    $result = mysql_query("SELECT COUNT(*) FROM $table", $db);
-                    $result = mysql_fetch_row($result);
-                    if ( $result[0] > $this->max_cache_rows ) {
-                        mysql_query("DELETE FROM $table WHERE expiration < DATE_SUB(NOW(), INTERVAL $cache_expire second)", $db);
-                        mysql_query('OPTIMIZE TABLE ' . $this->cache_table, $db);
-                    }
-                    $this->cache = 'db';
-                    $this->cache_db = $db;
-                    $this->cache_table = $table;
-                }
-            } elseif ($type == 'fs') {
-                $this->cache = 'fs';
-                $connection = realpath($connection);
-                $this->cache_dir = $connection;
-                if ($dir = opendir($this->cache_dir)) {
-                    while ($file = readdir($dir)) {
-                        if (substr($file, -6) == '.cache' && ((filemtime($this->cache_dir . '/' . $file) + $cache_expire) < time()) ) {
-                            unlink($this->cache_dir . '/' . $file);
-                        }
-                    }
-                }
-            } elseif ( $type == 'custom' ) {
-                $this->cache = "custom";
-                $this->custom_cache_get = $connection[0];
-                $this->custom_cache_set = $connection[1];
-            }
-            $this->cache_expire = $cache_expire;
-        }
-
-        function getCached ($request)
-        {
-            //Checks the database or filesystem for a cached result to the request.
-            //If there is no cache result, it returns a value of false. If it finds one,
-            //it returns the unparsed XML.
-            foreach ( $request as $key => $value ) {
-                if ( empty($value) ) unset($request[$key]);
-                else $request[$key] = (string) $request[$key];
-            }
-            //if ( is_user_logged_in() ) print_r($request);
-            $reqhash = md5(serialize($request));
-            $this->cache_key = $reqhash;
-            $this->cache_request = $request;
-            if ($this->cache == 'db') {
-                $result = mysql_query("SELECT response FROM " . $this->cache_table . " WHERE request = '" . $reqhash . "' AND DATE_SUB(NOW(), INTERVAL " . (int) $this->cache_expire . " SECOND) < expiration", $this->cache_db);
-                if ( mysql_num_rows($result) ) {
-                    $result = mysql_fetch_assoc($result);
-                    return $result['response'];
-                } else {
-                    return false;
-                }
-            } elseif ($this->cache == 'fs') {
-                $file = $this->cache_dir . '/' . $reqhash . '.cache';
-                if (file_exists($file)) {
-                    if ($this->php_version[0] > 4 || ($this->php_version[0] == 4 && $this->php_version[1] >= 3)) {
-                        return file_get_contents($file);
-                    } else {
-                        return implode('', file($file));
-                    }
-                }
-            } elseif ( $this->cache == 'custom' ) {
-                return call_user_func_array($this->custom_cache_get, array($reqhash));
-            }
-            return false;
-        }
-
-        function cache ($request, $response)
-        {
-            //Caches the unparsed response of a request.
-            unset($request['api_sig']);
-            foreach ( $request as $key => $value ) {
-                if ( empty($value) ) unset($request[$key]);
-                else $request[$key] = (string) $request[$key];
-            }
-            $reqhash = md5(serialize($request));
-            if ($this->cache == 'db') {
-                //$this->cache_db->query("DELETE FROM $this->cache_table WHERE request = '$reqhash'");
-                $result = mysql_query("SELECT COUNT(*) FROM " . $this->cache_table . " WHERE request = '" . $reqhash . "'", $this->cache_db);
-                $result = mysql_fetch_row($result);
-                if ( $result[0] ) {
-                    $sql = "UPDATE " . $this->cache_table . " SET response = '" . str_replace("'", "''", $response) . "', expiration = '" . strftime("%Y-%m-%d %H:%M:%S") . "' WHERE request = '" . $reqhash . "'";
-                    mysql_query($sql, $this->cache_db);
-                } else {
-                    $sql = "INSERT INTO " . $this->cache_table . " (request, response, expiration) VALUES ('$reqhash', '" . str_replace("'", "''", $response) . "', '" . strftime("%Y-%m-%d %H:%M:%S") . "')";
-                    mysql_query($sql, $this->cache_db);
-                }
-            } elseif ($this->cache == "fs") {
-                $file = $this->cache_dir . "/" . $reqhash . ".cache";
-                $fstream = fopen($file, "w");
-                $result = fwrite($fstream,$response);
-                fclose($fstream);
-                return $result;
-            } elseif ( $this->cache == "custom" ) {
-                return call_user_func_array($this->custom_cache_set, array($reqhash, $response, $this->cache_expire));
-            }
-            return false;
-        }
-
         function setCustomPost ( $function ) {
             $this->custom_post = $function;
         }
+
 
         function post ($data, $type = null) {
             if ( is_null($type) ) {
@@ -273,14 +132,14 @@ if ( !class_exists('afgFlickr') ) {
             return $response;
         }
 
-        function request ($command, $args = array(), $nocache = false)
+        function request ($command, $args = array())
         {
-            //Sends a request to Flickr's REST endpoint via POST.
+            // Sends a request to Flickr's REST endpoint via POST.
             if (substr($command,0,7) != "flickr.") {
                 $command = "flickr." . $command;
             }
 
-            //Process arguments, including method and login data.
+            // Process arguments, including method and login data.
             $args = array_merge(array("method" => $command, "format" => "php_serial", "api_key" => $this->api_key), $args);
             if (!empty($this->token)) {
                 $args = array_merge($args, array("auth_token" => $this->token));
@@ -290,21 +149,18 @@ if ( !class_exists('afgFlickr') ) {
             ksort($args);
             $auth_sig = "";
             $this->last_request = $args;
-            if (!($this->response = $this->getCached($args)) || $nocache) {
-                foreach ($args as $key => $data) {
-                    if ( is_null($data) ) {
-                        unset($args[$key]);
-                        continue;
-                    }
-                    $auth_sig .= $key . $data;
+            foreach ($args as $key => $data) {
+                if ( is_null($data) ) {
+                    unset($args[$key]);
+                    continue;
                 }
-                if (!empty($this->secret)) {
-                    $api_sig = md5($this->secret . $auth_sig);
-                    $args['api_sig'] = $api_sig;
-                }
-                $this->response = $this->post($args);
-                $this->cache($args, $this->response);
+                $auth_sig .= $key . $data;
             }
+            if (!empty($this->secret)) {
+                $api_sig = md5($this->secret . $auth_sig);
+                $args['api_sig'] = $api_sig;
+            }
+            $this->response = $this->post($args);
 
             /*
              * Uncomment this line (and comment out the next one) if you're doing large queries
@@ -397,13 +253,6 @@ if ( !class_exists('afgFlickr') ) {
             return $url;
         }
 
-        function getFriendlyGeodata ($lat, $lon) {
-            /* I've added this method to get the friendly geodata (i.e. 'in New York, NY') that the
-             * website provides, but isn't available in the API. I'm providing this service as long
-             * as it doesn't flood my server with requests and crash it all the time.
-             */
-            return unserialize(file_get_contents('https://phpflickr.com/geodata/?format=php&lat=' . $lat . '&lon=' . $lon));
-        }
 
         function sync_upload ($photo, $title = null, $description = null, $tags = null, $is_public = null, $is_friend = null, $is_family = null) {
             if ( function_exists('curl_init') ) {
@@ -1722,11 +1571,7 @@ if ( !class_exists('afgFlickr_pager') ) {
         function set_afgFlickr($afgFlickr) {
             if ( is_a($afgFlickr, 'afgFlickr') ) {
                 $this->afgFlickr = $afgFlickr;
-                if ( $this->afgFlickr->cache ) {
-                    $this->args['per_page'] = 500;
-                } else {
-                    $this->args['per_page'] = (int) $this->per_page;
-                }
+                $this->args['per_page'] = (int) $this->per_page;
             }
         }
 
@@ -1747,56 +1592,17 @@ if ( !class_exists('afgFlickr_pager') ) {
             );
             if ( !in_array($this->method, array_keys($allowed_methods)) ) return false;
 
-            if ( $this->afgFlickr->cache ) {
-                $min = ($page - 1) * $this->per_page;
-                $max = $page * $this->per_page - 1;
-                if ( floor($min/500) == floor($max/500) ) {
-                    $this->args['page'] = floor($min/500) + 1;
-                    $this->results = $this->afgFlickr->call($this->method, $this->args);
-                    if ( $this->results ) {
-                        $this->results = $this->results[$allowed_methods[$this->method]];
-                        $this->photos = array_slice($this->results['photo'], $min % 500, $this->per_page);
-                        $this->total = $this->results['total'];
-                        $this->pages = ceil($this->results['total'] / $this->per_page);
-                        return true;
-                    } else {
-                        return false;
-                    }
-                } else {
-                    $this->args['page'] = floor($min/500) + 1;
-                    $this->results = $this->afgFlickr->call($this->method, $this->args);
-                    if ( $this->results ) {
-                        $this->results = $this->results[$allowed_methods[$this->method]];
+            $this->args['page'] = $page;
+            $this->results = $this->afgFlickr->call($this->method, $this->args);
+            if ( $this->results ) {
+                $this->results = $this->results[$allowed_methods[$this->method]];
 
-                        $this->photos = array_slice($this->results['photo'], $min % 500);
-                        $this->total = $this->results['total'];
-                        $this->pages = ceil($this->results['total'] / $this->per_page);
-
-                        $this->args['page'] = floor($min/500) + 2;
-                        $this->results = $this->afgFlickr->call($this->method, $this->args);
-                        if ( $this->results ) {
-                            $this->results = $this->results[$allowed_methods[$this->method]];
-                            $this->photos = array_merge($this->photos, array_slice($this->results['photo'], 0, $max % 500 + 1));
-                        }
-                        return true;
-                    } else {
-                        return false;
-                    }
-
-                }
+                $this->photos = $this->results['photo'];
+                $this->total = $this->results['total'];
+                $this->pages = $this->results['pages'];
+                return true;
             } else {
-                $this->args['page'] = $page;
-                $this->results = $this->afgFlickr->call($this->method, $this->args);
-                if ( $this->results ) {
-                    $this->results = $this->results[$allowed_methods[$this->method]];
-
-                    $this->photos = $this->results['photo'];
-                    $this->total = $this->results['total'];
-                    $this->pages = $this->results['pages'];
-                    return true;
-                } else {
-                    return false;
-                }
+                return false;
             }
         }
 
