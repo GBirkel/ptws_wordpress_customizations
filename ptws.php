3a2bd97d7af226463053197ecf758384ca46a81f
Garrett Birkel <gbirkel@me.com>
2024-04-10 13:56:28 -0700

    Links to icons in page headers.

--- ptws.php
+++ ptws.php
@@ -422,6 +422,34 @@ function ptws_enqueue_styles()
 }
 
 
+function ptws_additional_page_head() {
+
+    // Post object if needed
+    //global $post;
+    // Page conditional if needed
+    //if( is_page() ){}
+    $icons_url = PTWS_PLUGIN_URL . "/images/site_icons/";
+    ?>
+        <link rel="shortcut icon" href="<?php echo $icons_url ?>favicon.ico">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $icons_url ?>favicon-16x16.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $icons_url ?>favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="48x48" href="<?php echo $icons_url ?>favicon-48x48.png">
+        <link rel="apple-touch-icon" sizes="57x57" href="<?php echo $icons_url ?>apple-touch-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="<?php echo $icons_url ?>apple-touch-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="<?php echo $icons_url ?>apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="<?php echo $icons_url ?>apple-touch-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="<?php echo $icons_url ?>apple-touch-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="<?php echo $icons_url ?>apple-touch-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="<?php echo $icons_url ?>apple-touch-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="<?php echo $icons_url ?>apple-touch-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="167x167" href="<?php echo $icons_url ?>apple-touch-icon-167x167.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $icons_url ?>apple-touch-icon-180x180.png">
+        <link rel="apple-touch-icon" sizes="1024x1024" href="<?php echo $icons_url ?>apple-touch-icon-1024x1024.png">    
+    <?php
+
+}
+
+
 function ptws_auth_read()
 {
     if (isset($_GET['frob'])) {
@@ -450,6 +478,7 @@ function ptws_activate()
     add_option('ptws_plugin_activation', 'just-activated');
 }
 
+add_action('wp_head', __NAMESPACE__ . '\ptws_additional_page_head' );
 add_action('wp_print_scripts', __NAMESPACE__ . '\ptws_enqueue_scripts');
 add_action('wp_print_styles', __NAMESPACE__ . '\ptws_enqueue_styles');
 
