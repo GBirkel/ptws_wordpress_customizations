a97e300eab908e00663892b485cc703bcdf774b7
Garrett Birkel <gbirkel@me.com>
2025-05-10 12:37:58 -0700

    WHoops!

--- blocks/slides/block.js
+++ blocks/slides/block.js
@@ -490,7 +490,7 @@
 						var fixedOrSwipe = attributes?.named?.swipe ? "swipe" : "fixed";
 						// If none are found in attributes, search the shortcode body
 						if (!ids) {
-							const idsMatch = shortcodeMatch?.content?.match(/\s+"[\d\s]+"/gi);
+							const idsMatch = shortcodeMatch?.content?.match(/"[\d\s]+"/gi);
 							if (idsMatch) {
 								ids = idsMatch.map((m) => m.match(/[\d]+/)[0]).join(',');
 								fixedOrSwipe = shortcodeMatch?.content?.match(/fixedgallery/gi) ? "fixed" : "swipe";
