eeff1001d43f199a2c106ef2f8ca05a4e6733a06
Garrett Birkel <gbirkel@me.com>
2026-03-17 18:09:38 -0700

    Removing some unused code

--- ptws-blocks.php
+++ ptws-blocks.php
@@ -35,40 +35,6 @@ function register_dynamic_blocks()
     register_block_type(PTWS_PLUGIN_DIRECTORY . '/blocks/slides-flickr');
     register_block_type(PTWS_PLUGIN_DIRECTORY . '/blocks/slides-stack');
 
-    // Hook server side rendering into render callback
-    //register_block_type('ptws/gallery', [
-    //    'name' => 'PTWS Gallery',
-    //    'description' => 'A server-side assembled gallery of images with various options.',
-    //    'render_callback' => __NAMESPACE__ . '\render_dynamic_gallery_block',
-    //    'category' => 'widgets',
-    //]);
 }
-
-
-/**
- * Server rendering for dynamic block
- */
-function render_dynamic_gallery_block($block)
-{
-    $recent_posts = wp_get_recent_posts([
-        'numberposts' => 3,
-        'post_status' => 'publish',
-    ]);
-
-    if (empty($recent_posts)) { return '<p>No posts</p>'; }
-
-    $markup = '<ul>';
-    foreach ($recent_posts as $post) {
-        $post_id  = $post['ID'];
-        $markup  .= sprintf(
-            '<li><a href="%1$s">%2$s</a></li>',
-            esc_url(get_permalink($post_id)),
-            esc_html(get_the_title($post_id))
-        );
-    }
-    $block_description = print_r($block, true);
-    return "{$markup}</ul>{$block_description}<p>poop</p>";
-}
-
 
 ?>
